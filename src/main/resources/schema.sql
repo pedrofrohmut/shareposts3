b218ec90eff72a25ba4d0c5435c657be8dd8f21b
Pedro Frohmut <pedrofrohmut@gmail.com>
2018-08-22 17:03:59 -0300

    a

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE shareposts
+	CHARSET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS shareposts.user;
+
+DROP TABLE IF EXISTS shareposts.post;
+
+CREATE TABLE shareposts.user (
+	id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE shareposts.post (
+	id INT AUTO_INCREMENT NOT NULL,
+	user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
